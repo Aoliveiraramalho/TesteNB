dfb3ecb5c35a2d6d694a83da1298bff1ebb6495c
Aoliveiraramalho <157823526+Aoliveiraramalho@users.noreply.github.com>
2024-08-06 13:58:17 -0300

    Add files via upload

--- ApplicationMedia/src/applicationmedia/ApplicationMedia.java
+++ ApplicationMedia/src/applicationmedia/ApplicationMedia.java
@@ -8,7 +8,7 @@ public class ApplicationMedia {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         String nome;
-        double nota1, nota2, nota3, media;
+        double nota1, nota2, nota3, nota4, media;
         System.out.println("Digite o nome do Aluno");
         nome = ler.nextLine();
         System.out.println("Digite Nota 1: ");
@@ -17,7 +17,9 @@ public class ApplicationMedia {
         nota2 = ler.nextDouble();
         System.out.println("Digite Nota 3: ");
         nota3 = ler.nextDouble();
-        media = (nota1 + nota2 + nota3) / 3;
+        System.out.println("Digite Nota 4: ");
+        nota4 = ler.nextDouble();
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
         System.out.println("Nome do Aluno: " + nome);
         System.out.printf("Nota 1 = %.2f %n", nota1);
         System.out.printf("Nota 2 = %.2f %n", nota2);
