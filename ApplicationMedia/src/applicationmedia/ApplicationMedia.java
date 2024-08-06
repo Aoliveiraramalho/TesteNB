f11ba83f01e99b2b9ff0bfef109b42b3db0cd6af
Aoliveiraramalho <157823526+Aoliveiraramalho@users.noreply.github.com>
2024-08-06 13:40:34 -0300

    Add files via upload

---
+++ ApplicationMedia/src/applicationmedia/ApplicationMedia.java
@@ -0,0 +1,29 @@
+
+package applicationmedia;
+
+import java.util.Scanner;
+
+public class ApplicationMedia {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        String nome;
+        double nota1, nota2, nota3, media;
+        System.out.println("Digite o nome do Aluno");
+        nome = ler.nextLine();
+        System.out.println("Digite Nota 1: ");
+        nota1 = ler.nextDouble();
+        System.out.println("Digite Nota 2: ");
+        nota2 = ler.nextDouble();
+        System.out.println("Digite Nota 3: ");
+        nota3 = ler.nextDouble();
+        media = (nota1 + nota2 + nota3) / 3;
+        System.out.println("Nome do Aluno: " + nome);
+        System.out.printf("Nota 1 = %.2f %n", nota1);
+        System.out.printf("Nota 2 = %.2f %n", nota2);
+        System.out.printf("Nota 3 = %.2f %n", nota3);
+        System.out.printf("Média = %.2f %n", media);
+        ler.close();
+    }
+    
+}
